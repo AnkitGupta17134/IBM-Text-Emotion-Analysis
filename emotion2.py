929e9b384eb694cb8ec031e84a4e5087b5c18269
regexistaken <32675731+AnkitGupta17134@users.noreply.github.com>
2019-05-22 01:23:19 +0530

    Main Python Code for Emotion Recognition.

---
+++ emotion2.py
@@ -0,0 +1,18 @@
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 \
+    import Features, EntitiesOptions, KeywordsOptions
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2018-11-16',
+    iam_apikey='QqhqLPufmUFVNLZaMvrkzvtKqHDVsfZDLF8cUNEHaaqL',
+    url='https://gateway-lon.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2018-11-16')
+
+response = natural_language_understanding.analyze(
+    text='I Love Deepika. I hate curd. He is stupid.',
+    features=Features(
+        entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
+        keywords=KeywordsOptions(emotion=True, sentiment=True,
+                                 limit=2))).get_result()
+
+print(json.dumps(response, indent=2))
